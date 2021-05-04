78d894c427a090cbac392e04345d7171c0b2aa64
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2021-05-04 14:07:44 +0100

    updated db file

--- monthlies_db.sql
+++ monthlies_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2021 at 02:03 PM
+-- Generation Time: May 04, 2021 at 03:05 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -30,13 +30,51 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `first name` text NOT NULL,
-  `last name` text NOT NULL,
+  `fullname` text NOT NULL,
   `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `date_joined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `age` int(11) NOT NULL,
   `location` text NOT NULL,
-  `phone_number` int(13) NOT NULL,
-  `password` varchar(256) NOT NULL
+  `marital_status` text NOT NULL,
+  `address` text NOT NULL,
+  `phone_number` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `email`, `password`, `date_joined`, `age`, `location`, `marital_status`, `address`, `phone_number`) VALUES
+(1, 'shittu olumide ayodeji', 'iamholumeedey007@gmail.com', 'olumide', '2021-05-04 12:59:45', 18, 'Ondo State', 'Single', 'new site area c isusda owo.', '14242753278'),
+(2, 'Yisa Dedorah', 'yisadebby@gmail.com', 'Iyanu15#', '2021-04-27 02:30:49', 23, 'Kaduna, Nigeria.', 'Married', 'Number 1, Yuhnuda Kaduna', '9061183812');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cycle_weight` text NOT NULL,
+  `date_last_seen` date NOT NULL,
+  `duration` text NOT NULL,
+  `cycle_length` int(11) NOT NULL,
+  `cycle_problems` text NOT NULL,
+  `predicted_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`id`, `user_id`, `cycle_weight`, `date_last_seen`, `duration`, `cycle_length`, `cycle_problems`, `predicted_date`) VALUES
+(2, 1, 'light', '2021-05-29', '5', 25, 'liMenstrual Crampsght', '2021-06-23'),
+(3, 1, 'heavy', '2021-05-11', '5', 24, 'Mood Changes', '2021-06-04'),
+(5, 1, 'light', '2021-05-20', '3', 24, 'Pains in lower abdomen', '2021-06-13'),
+(6, 1, 'heavy', '2021-02-16', '5', 24, 'Acne', '2021-03-12');
 
 --
 -- Indexes for dumped tables
@@ -49,6 +87,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -56,7 +100,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
